4e7bce7a977fc331d1f564eb4fa051452355318a
SY Ryu <bbs0605@naver.com>
2023-11-28 17:12:24 +0900

    힌트 2 내용 추가.

--- lib/11_28/team_challenge_by_teacher.dart
+++ lib/11_28/team_challenge_by_teacher.dart
@@ -1,4 +1,4 @@
-//흰트 1 공개 내용.
+//Hint 2 내용 추가.
 main() {
   List<String> players = ['Player1', 'Player2', 'Player3', 'Player4'];
 
@@ -10,7 +10,24 @@ main() {
   List<int> clapCountsByPlayer = [0, 0, 0, 0];
 
   for (int i = 1; i <= 100; i++) {
-    int playIndex = (i - 1) % 4;
     String currentPlayer = players[currentPlayerIndex];
+
+    if (i % 30 == 0) {
+      ahhCount++;
+      print("$currentPlayer : ahh");
+    } else if (i % 3 == 0) {
+      clapCount++;
+      print("$currentPlayer : clap");
+    } else if (i % 10 == 0) {
+      roolCount++;
+      print("$currentPlayer : rool");
+    } else {
+      print("$currentPlayer : $i");
+    }
+    currentPlayerIndex = (currentPlayerIndex + 1) % players.length;
   }
+
+  print("\nclap 의 총 갯수 : ${clapCount}");
+  print("rool 의 총 갯수 : ${roolCount}");
+  print("ahh 의 총 갯수 : ${ahhCount}");
 }

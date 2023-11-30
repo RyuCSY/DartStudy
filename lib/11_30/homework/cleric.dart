2d0090b8e42a782539631d133bacf047c758cf90
SY Ryu <bbs0605@naver.com>
2023-11-30 16:53:06 +0900

    연습문제 1-4

--- lib/11_30/homework/cleric.dart
+++ lib/11_30/homework/cleric.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 class Cleric {
   static const int MAX_OF_HP = 50;
   static const int MAX_OF_MP = 10;
@@ -20,5 +22,25 @@ class Cleric {
       mp -= 5;
       hp = MAX_OF_HP;
     }
+  }
+
+  /**
+   * 성직자는 “기도하기" (pray) 라는 행동을 취할 수 있고, 자신의 MP를 회복한다.
+      회복량은 기도한 시간(초)에 랜덤하게 0 ~ 2포인트의 보정을 한 양이다 (3초 기도하면 회복량은 3 ~ 5 포인트). 단, 최대 MP 보다 더 회복하는 것은 불가능 하다.
+      연습 1-3에서 선언한 Cleric 클래스에 “pray()” 메소드를 추가하시오.
+      이 메소드는 인수에 “기도할 시간(초)"를 지정할 수 있고, 리턴 값은 “실제로 회복된 MP 양" 을 반환한다.
+   */
+  int pray(int sec) {
+    int ret = (Random().nextInt(3) + sec);
+    if (MAX_OF_MP < ret) {
+      ret = MAX_OF_MP;
+    }
+
+    mp += ret;
+
+    if (MAX_OF_MP < mp) {
+      mp = MAX_OF_MP;
+    }
+    return ret;
   }
 }

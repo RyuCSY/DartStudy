8adad15b64dc001a610541954a3331c8d329dc93
SY Ryu <bbs0605@naver.com>
2023-12-04 14:29:37 +0900

    연습문제 10-1.
    다음 2개의 클래스 “Wizard (마법사)”, “Wand (지팡이)” 를 작성하시오. 마법사는 지팡이를 들 수 있습니다.

---
+++ lib/12_04/encapsulation/homework/wand.dart
@@ -0,0 +1,13 @@
+/// 연습문제 10-1. 다음 2개의 클래스 “Wizard (마법사)”, “Wand (지팡이)” 를 작성하시오. 마법사는 지팡이를 들 수 있습니다.
+class Wand {
+  // 이름
+  String name;
+
+  // 마력
+  double power;
+
+  Wand({
+    required this.name,
+    required this.power,
+  });
+}

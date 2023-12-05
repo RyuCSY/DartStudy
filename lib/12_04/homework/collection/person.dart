acbaabb661b8ca0b5caa2fb4ec635648a0e70491
SY Ryu <bbs0605@naver.com>
2023-12-05 09:21:20 +0900

    컬렉션 연습문제 2-3

--- lib/12_04/homework/collection/person.dart
+++ lib/12_04/homework/collection/person.dart
@@ -15,4 +15,18 @@ main() {
   list.forEach((person) {
     print(person.name);
   });
+
+  // 연습문제 2-3
+  //
+  // 연습문제 2-3 에서 작성한 Person 클래스로 생성한 ‘홍길동’, ‘한석봉'의 나이를 각각 20, 25살 이라고 할 때, 이름과 나이를 쌍으로 적당한 컬렉션에 넣습니다.
+  Map<Person, int> personMap = {
+    list[0]: 20,
+    list[1]: 25,
+  };
+
+  // 그 다음, 컬렉션에 저장한 값을 하나씩 다음과 같이 출력합니다.
+  // “홍길동의 나이는 20살”“한석봉의 나이는 25살”
+  personMap.forEach((person, age) {
+    print('"${person.name}의 나이는 $age살"');
+  });
 }
